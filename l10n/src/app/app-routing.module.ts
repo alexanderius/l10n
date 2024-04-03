5e9b0cde6dabe762b36571be3d12eaf744e06fef
Alexander Bylinsky <a.bylinsky@alverden.com>
2024-04-03 19:19:39 +0300

    Routing draft

--- l10n/src/app/app-routing.module.ts
+++ l10n/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TeamComponent } from './team/team.component';
+import { ProjectsComponent } from './team/projects/projects.component';
+import { ProjectComponent } from './team/projects/project/project.component';
+import { TeamsComponent } from './teams/teams.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'teams', component: TeamsComponent },
+  {
+    path: 'teams/:teamId', component: TeamComponent,
+    children: [
+      { path: 'projects', component: ProjectsComponent }, 
+      { path: 'projects/:projectId', component: ProjectComponent }, 
+      { path: 'projects', redirectTo: 'teams/:teamId/projects/default/', pathMatch: 'full' }
+    ]
+  }, 
+  { path: '**', redirectTo: 'teams/default/projects/default/', pathMatch: 'full' },
+  { path: '', redirectTo: 'teams', pathMatch: 'full' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- l10n/src/app/app.module.ts
+++ l10n/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TeamComponent } from './team/team.component';
+import { ProjectsComponent } from './team/projects/projects.component';
+import { ProjectComponent } from './team/projects/project/project.component';
+import { TeamsComponent } from './teams/teams.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamComponent,
+    ProjectsComponent,
+    ProjectComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
