67319414014a38ec5520d1e04fbac02317462b0c
Alex Bylinsky <a.bylinsky@alverden.com>
2024-04-05 00:49:30 +0300

    Draft structure of the table with translations

--- l10n/src/app/team/project/project.component.ts
+++ l10n/src/app/team/project/project.component.ts
@@ -1,11 +1,24 @@
+import { CommonModule, NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
 
+interface LocalizationKey {
+  id: string;
+  children?: LocalizationKey[];
+}
+
 @Component({
-    selector: 'app-project',
-    templateUrl: './project.component.html',
-    styleUrls: ['./project.component.scss'],
-    standalone: true
+  imports: [CommonModule, NgTemplateOutlet],
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.scss'],
+  standalone: true
 })
 export class ProjectComponent {
-
+  locales: any[] = ['en-US', 'es-ES', 'de-DE'];
+  keys: LocalizationKey[] = [{
+    id: 'public',
+    children: [
+      { id: 'signIn' },
+      { id: 'signUp' },
+    ]
+  }];
 }
