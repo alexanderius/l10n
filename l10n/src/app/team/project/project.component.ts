2921c06e9f1f5508266f50fe736da3ba77497144
Alex Bylinsky <a.bylinsky@alverden.com>
2024-04-06 10:06:10 +0300

    Updated table layout

--- l10n/src/app/team/project/project.component.ts
+++ l10n/src/app/team/project/project.component.ts
@@ -13,12 +13,66 @@ interface LocalizationKey {
   standalone: true
 })
 export class ProjectComponent {
-  locales: any[] = ['en-US', 'es-ES', 'de-DE'];
+  locales: any[] = ['en-US', 'es-ES', 'de-DE', 'en-US', 'es-ES','en-US', 'es-ES','en-US', 'es-ES','en-US', 'es-ES','en-US', 'es-ES','en-US', 'es-ES','en-US', 'es-ES','en-US', 'es-ES','en-US', 'es-ES', 'ru-RU'];
   keys: LocalizationKey[] = [{
     id: 'public',
     children: [
       { id: 'signIn' },
       { id: 'signUp' },
+      { id: 'signIn1' },
+      { id: 'signUp2' },
+      { id: 'signIn3' },
+      { id: 'signUp4' },
+      { id: 'signIn5' },
+      { id: 'signUp6' },
+      { id: 'signIn7' },
+      { id: 'signUp8' },
+      { id: 'signIn9' },
+      { id: 'signUp10' },
+      { id: 'signIn11' },
+      { id: 'signUp12' },
+      { id: 'signIn13' },
+      { id: 'signUp14' },
+      { id: 'signIn15' },
+      { id: 'signUp16' },
+      { id: 'signIn17' },
+      { id: 'signUp18' },
+      { id: 'signIn19' },
+      { id: 'signUp20' },
+      { id: 'signIn21' },
+      { id: 'signUp22' },
+      { id: 'signIn23' },
+      { id: 'signUp24' },
+    ]
+  }, {
+    id: 'private',
+    children: [
+      { id: 'signIn' },
+      { id: 'signUp' },
+      { id: 'signIn1' },
+      { id: 'signUp2' },
+      { id: 'signIn3' },
+      { id: 'signUp4' },
+      { id: 'signIn5' },
+      { id: 'signUp6' },
+      { id: 'signIn7' },
+      { id: 'signUp8' },
+      { id: 'signIn9' },
+      { id: 'signUp10' },
+      { id: 'signIn11' },
+      { id: 'signUp12' },
+      { id: 'signIn13' },
+      { id: 'signUp14' },
+      { id: 'signIn15' },
+      { id: 'signUp16' },
+      { id: 'signIn17' },
+      { id: 'signUp18' },
+      { id: 'signIn19' },
+      { id: 'signUp20' },
+      { id: 'signIn21' },
+      { id: 'signUp22' },
+      { id: 'signIn23' },
+      { id: 'signUp24' },
     ]
   }];
 }
