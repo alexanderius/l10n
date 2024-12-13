3e2da2f1f7cdb5cfd95349894b4e963e78032d65
Alex Bylinsky <a.bylinsky@alverden.com>
2024-12-13 12:52:41 +0300

    Added draft structure of pages

---
+++ src/app/team/projects/projects.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss'],
+  standalone: true
+})
+export class ProjectsComponent {
+
+}
---
+++ src/app/team/team.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+    selector: 'app-team',
+    templateUrl: './team.component.html',
+    styleUrls: ['./team.component.scss'],
+    standalone: true,
+    imports: [RouterOutlet]
+})
+export class TeamComponent {
+
+}
