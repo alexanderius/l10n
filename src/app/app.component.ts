6b14ee937945c0de5863b1c0f9599457f1f68615
Reut Vladislav <reut-2011@mail.ru>
2025-02-20 12:42:26 +0300

    feat: add auth provider and autorization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
+import { CommonModule } from '@angular/common';
+
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'l10n';
+
+  constructor(public auth: AuthService) {}
+
+  ngOnInit(): void {
+    this.auth.isAuthenticated$.pipe(take(1)).subscribe((isAuthenticated) => {
+      if (!isAuthenticated) {
+        this.auth.loginWithRedirect({
+          appState: { target: '/teams/default/projects' },
+        });
+      }
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,27 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+
+    provideAuth0({
+      domain: 'localization-l10n.us.auth0.com',
+      clientId: 'VQ7Dzo4nhRbbfHgYECxeHFnv9wjtm9kV',
+      authorizationParams: {
+        // redirect_uri: window.location.origin,
+        redirect_uri: 'http://localhost:4200/teams/default/projects',
+        audience: 'https://api.localization-l10n.com',
+      },
+    }),
+  ],
 };
